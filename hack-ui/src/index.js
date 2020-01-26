0dee2e1f361a5c38bb3f04ea31577394691aadaf
as-sah <as_sah@live.concordia.ca>
2020-01-25 21:36:40 -0500

    added login

--- hack-ui/src/index.js
+++ hack-ui/src/index.js
@@ -3,8 +3,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//import App from './App';
-import LoginPage from "./LoginPage";
+import NavBar from "./components/NavBar";
 import "./index.css";
 
-ReactDOM.render(<LoginPage />, document.getElementById("root"));
+ReactDOM.render(<NavBar />, document.getElementById("root"));
