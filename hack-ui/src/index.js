e0feb550c993b112e88e3751b21047b0d7401786
as-sah <as_sah@live.concordia.ca>
2020-01-25 21:29:40 -0500

    added loginpage

--- hack-ui/src/index.js
+++ hack-ui/src/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+/** @format */
 
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//import App from './App';
+import LoginPage from "./LoginPage";
+import "./index.css";
+
+ReactDOM.render(<LoginPage />, document.getElementById("root"));
